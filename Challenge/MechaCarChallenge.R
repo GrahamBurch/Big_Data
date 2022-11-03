34919e5fdab1cc6c482235558ef1cf31a11697b7
Graham Burch <grahamwburch@gmail.com>
2022-11-03 18:35:17 -0400

    Updating code and adding images

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -3,9 +3,36 @@ library(dplyr)
 library(tidyverse)
 
 # Read in MechaCar dataset
-mechaCar_df ad.csv('MechaCar_mpg.csv', check.names= F, stringsAsFactors = F)
+mechaCar_df <- read.csv('Challenge/MechaCar_mpg.csv', check.names= F, stringsAsFactors = F)
 mechaCar_df
 
 # Calculate linear model for dataset
-line <-
+mecha_line <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_df)
+summary(mecha_line)
 
+
+
+# Reading in Suspension_Coil dataset
+coil_df <- read.csv('Challenge/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Create total_summary dataframe using the summarize function
+total_summary
+
+lot_summary <- coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Group each manufacturing lot by the mean, median, variance, and st.dv of the suspsion coil's PSI column.
+lot_summary
+
+
+# Use t.test() on PSI over all manufacturing lots
+t.test(lot_summary$Mean, mu=1500)
+
+# Test only lot 1
+lot1_summary <- subset(coil_df, Manufacturing_Lot == 'Lot1')
+t.test(lot1_summary$PSI, mu=1500)
+
+# Test only lot 2
+lot2_summary <- subset(coil_df, Manufacturing_Lot == 'Lot2')
+t.test(lot2_summary$PSI, mu=1500)
+
+# Test only lot 3
+lot3_summary <- subset(coil_df, Manufacturing_Lot == 'Lot3')
+t.test(lot3_summary$PSI, mu=1500)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Big_Data
+# MechaCar Statistical Analysis
+
+## Overview
+* This analysis investigates MechaCar's car production data for potentially useful insights. We aim to isolate the particular variables most predictive of miles per gallon performance. 
+
+## Linear Regression
+* Spoiler weight, vehicle angle and AWD (all-wheel drive) contributed to non-random variances in the model. Ground clearance and vehicle length were the two most significant variables. 
+
+![Linear Model for Car Variables](mecha_line.jpg)
